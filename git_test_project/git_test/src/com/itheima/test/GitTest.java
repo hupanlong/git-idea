e21e971890dfff1f5c15cebb9f475d480c626cec
hupanlong <1341082883@qq.com>
2019-06-13 14:11:55 +0800

    第一次添加java项目

---
+++ git_test_project/git_test/src/com/itheima/test/GitTest.java
@@ -0,0 +1,17 @@
+package com.itheima.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("git_test_project");
+        System.out.println("git_test_project");
+        System.out.println("git_test_project");
+        System.out.println("git_test_project");
+        System.out.println("git_test_project");
+        System.out.println("git_test_project");
+        System.out  .println("git_test_project");
+        System.out.println("git_test_project");
+        System.out.println("git_test_project");
+    }
+}

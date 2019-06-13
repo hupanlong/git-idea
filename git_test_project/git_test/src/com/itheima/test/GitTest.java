731a2c885b9f18ef1f0a975dc397ea7285b27214
hupanlong <1341082883@qq.com>
2019-06-13 14:35:23 +0800

    修改了test

--- git_test_project/git_test/src/com/itheima/test/GitTest.java
+++ git_test_project/git_test/src/com/itheima/test/GitTest.java
@@ -15,9 +15,14 @@ public class GitTest {
         System.out.println("git_test_project");
     }
 
+    //修改了test
     public void test() {
         int i = 10;
         i += 20;
         System.out.println(i);
+        int s = 40;
+        System.out.println(i+s);
+        System.out.println("....");
+        System.out.println("显示");
     }
 }
